0ebb461819ff129c77e784b5f679b7efe7a51801
jessicacj <jessicacj@outlook.com>
2019-11-23 14:09:01 -0300

    add src

---
+++ src/app.js
@@ -0,0 +1,26 @@
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const index = require("./routes/index")
+const filmes = require("./routes/filmesRouter")
+
+const dataBase = require("./model/database")
+dataBase.connect()
+
+const app = express()
+
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*")
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+app.use(bodyParser.json())
+
+app.use("/", index)
+app.use('/filmes', filmes)
+
+module.exports = app
